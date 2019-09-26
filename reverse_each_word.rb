86457160bc6e337cd5f6c9dd8668fc5f771c9878
Tariq Bailey <tariqbailey627@gmail.com>
2019-09-26 02:03:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence1)
 sentence_array = sentence1.split(' ')
-reversed_array = sentence1.map{|str| str.reverse}
+reversed_array = sentence_array.map{|str| str.reverse}
 reversed_array.join(" ")
 end
